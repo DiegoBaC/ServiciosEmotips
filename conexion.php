878ea66d98a81fcd2594eb65fbb079e0b34557a4
DiegoBaC <diegobautis11@gmail.com>
2025-12-01 23:19:01 -0600

    Update conexion.php cambios

--- conexion.php
+++ conexion.php
@@ -1,14 +1,17 @@
 <?php
 header('Access-Control-Allow-Origin:*');
-$host = "yamabiko.proxy.rlwy.net";
+
+$host = "mysql.railway.internal"; 
 $user = "root";
 $pass = "bqEqlRXHPrqZCTxNZqppwxLjCvYqzbPM";
-$db = "railway";
+$db   = "railway";
 $port = 3306;
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
+
 $conn->set_charset("utf8mb4");
 ?>
